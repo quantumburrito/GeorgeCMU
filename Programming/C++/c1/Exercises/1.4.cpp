cc4fa57830f05e3f9beb7abab1c3ed221a573cce
quantumburrito <bfitzge@siue.edu>
2025-09-16 15:04:11 -0500

    finished 1.1-1.3

---
+++ Programming/C++/c1/Exercises/1.4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// main entry point
+int main() {
+    cout << "Please enter two numbers: ";
+    int in1, in2;
+    cin >> in1 >> in2;
+    cout << in1 << " * " << in2 << " = " << in1 * in2 << endl ;   
+    return 0;
+}
---
+++ Programming/C++/c1/Exercises/1.5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// main entry point
+int main() {
+    cout << "Please enter two numbers: ";
+    int in1, in2;
+    cin >> in1 >> in2;
+    cout << in1;
+    cout << " * ";
+    cout << in2;
+    cout << " = ";
+    cout  << in1 * in2;
+    cout << endl ;   
+    return 0;
+}
